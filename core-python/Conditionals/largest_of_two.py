8d7f60d86f94909dbd5553245f9df9a206568390
AryanSingh040403 <147248161+AryanSingh040403@users.noreply.github.com>
2026-08-02 07:41:46 +0530

    Add largest of the two numbers problem

---
+++ core-python/Conditionals/largest_of_two.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter First num = "))
+num2 = int(input("Enter Second num = "))
+if num1 == num2:
+    print("Both are equal")
+elif num1 < num2:
+    print("Second is larger")
+else:                               
+    print("First is larger")         
