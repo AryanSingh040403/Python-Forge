80729a2f14102c64ddb6ed1c6fdabdd82865ebb0
AryanSingh040403 <147248161+AryanSingh040403@users.noreply.github.com>
2026-08-02 07:16:30 +0530

    feat(Conditionals): add Positive , Negative or Zero  conditions

---
+++ core-python/Conditionals/pos_neg_zero.py
@@ -0,0 +1,8 @@
+num = int(input("Enter num to check whether it's positive, negative or zero = "))
+
+if num == 0:
+    print("Zero")
+elif num < 0:
+    print("negative")
+else:                               
+    print("Positive")         
