676a7b103da9bf1a7fbc8ea7548386055d6f74b9
Scarknight <aryansingh20030404@gmail.com>
2026-08-19 23:53:54 +0530

    Remove old employee bonus calculation logic
    Removed the old implementation for calculating employee bonuses based on experience and performance rating.

--- core-python/Conditionals/Phase-2/employee_bonus.py
+++ core-python/Conditionals/Phase-2/employee_bonus.py
@@ -19,19 +19,3 @@ else:
 
 # better/Cleaner alternative:
 
-years_of_experience = int(input())
-performance_rating= int(input())
-
-if years_of_experience < 0 or performance_rating < 1 or performance_rating > 5:
-    print("Invalid Input")
-elif years_of_experience >= 2:
-    if performance_rating == 1 or performance_rating == 2:
-        print("No Bonus")
-    elif performance_rating == 5:
-        print("20% Bonus")
-    elif performance_rating == 4: 
-        print("10% Bonus")
-    else:
-        print("5% Bonus")
-else:
-    print("No Bonus")
